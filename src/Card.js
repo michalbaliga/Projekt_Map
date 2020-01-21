3a0a032c0134b4c124f9b10942ef300705b2f35c
michalbaliga <michal_baliga@interia.pl>
2020-01-21 19:55:20 +0100

    Map

---
+++ src/Card.js
@@ -0,0 +1,19 @@
+// import React, { Component } from 'react';
+// import {Card, CardText, CardTitle, CardSubtitle, Button} from 'reactstrap';
+// import './Card.css';
+
+
+//   const Cards = (props) => {
+//     return (
+//       <div>
+//         <Card className="message">
+//             <CardTitle>Hello</CardTitle>
+//             <CardSubtitle>Card subtitle</CardSubtitle>
+//             <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+//             <Button>Button</Button>
+//         </Card>
+//       </div>
+//     );
+//   };
+  
+//   export default Cards;
